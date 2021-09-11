148223491b28010c5fa8d88952f727fe772e8d61
skeanster <ryanskeans@gmail.com>
2021-09-11 12:15:50 -0700

    added modal, javascript

---
+++ index.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector("#modal")
+const overlay = document.querySelector("#overlay") 
+const modalExit = document.querySelector("#modalExit")
+
+function activateModal() {
+    modal.classList.add("active")
+    overlay.classList.add("active")
+}
+
+function closeModal() {
+    modal.classList.remove("active")
+    overlay.classList.remove("active")
+}
