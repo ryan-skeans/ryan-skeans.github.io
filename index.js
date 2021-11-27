007febab3309b15d0d12643a97dec5e1ef4807df
skeanster <ryanskeans@gmail.com>
2021-11-27 12:29:19 -0800

    changed when the image background loads

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-document.body.onload = function () {
+document.getElementById('profile-desktop').onload = function () {
   document.getElementById('profile-desktop').classList.add('profile-desktop');
 };
