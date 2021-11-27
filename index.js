36bab37f89f06e39a3eb4b10191d768411351193
skeanster <ryanskeans@gmail.com>
2021-11-27 12:16:16 -0800

    added some fun styles

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const modal = document.querySelector("#modal");
-const overlay = document.querySelector("#overlay");
-const modalExit = document.querySelector("#modalExit");
-
-function activateModal() {
-  modal.classList.add("active");
-  overlay.classList.add("active");
-}
-
-function closeModal() {
-  modal.classList.remove("active");
-  overlay.classList.remove("active");
-}
+document.body.onload = function () {
+  document.getElementById('profile-desktop').classList.add('profile-desktop');
+};
