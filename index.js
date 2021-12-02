e3a34019e224f8b2f2e696f5aac92bd30a88d2c9
skeanster <ryanskeans@gmail.com>
2021-12-02 09:19:42 -0800

    ACTUALLY fixed count api

--- index.js
+++ index.js
@@ -3,8 +3,3 @@ let picture = document.getElementById('profile-desktop');
 setTimeout(function () {
   picture.classList.add('profile-desktop');
 }, 200);
-
-function liveViews(response) {
-  console.log(response.value);
-  console.log('test');
-}
