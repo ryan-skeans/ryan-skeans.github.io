76bb921b2c9bcd2799ee457250f2d654338785e6
skeanster <ryanskeans@gmail.com>
2021-12-02 08:57:32 -0800

    added visit tracking

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import countapi from './node_modules/countapi-js/lib/index';
+
 let picture = document.getElementById('profile-desktop');
 
 setTimeout(function () {
   picture.classList.add('profile-desktop');
 }, 200);
+
+countapi.visits().then((result) => {
+  console.log(result.value);
+});
