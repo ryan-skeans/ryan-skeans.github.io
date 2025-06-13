2e26b968ed91a8c8801d043822ca4715bdf7d7a3
Ryan Skeans <ryanskeans@gmail.com>
2025-06-13 13:53:27 -0700

    tweaks

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-let picture = document.getElementById('profile-desktop');
+document.addEventListener("DOMContentLoaded", () => {
+    const picture = document.getElementById("profile-desktop");
+    picture.classList.add("profile-desktop");
 
-setTimeout(function () {
-  picture.classList.add('profile-desktop');
-}, 0);
+    const sr = ScrollReveal({
+        distance: "200px",
+        duration: 600,
+        easing: "ease-out",
+        mobile: true,
+        reset: false,
+    });
+
+    sr.reveal(".skillsCard", {
+        origin: "left",
+        interval: 100,
+    });
+    sr.reveal(".toolsCard", {
+        origin: "right",
+        interval: 100,
+    });
+
+    sr.reveal(".project", {
+        origin: "bottom",
+        distance: "300px",
+        duration: 600,
+        easing: "ease-out",
+        interval: 200,
+        viewFactor: 0.2,
+    });
+});
